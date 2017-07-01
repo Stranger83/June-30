1b636e7a4e0153916333211dfb4005ff56b102ae
July Manova <jmanova83@gmail.com>
2017-07-01 15:26:35 +0300

    01,02/08 solved

---
+++ Censorship/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Censorship
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var word = Console.ReadLine();
+			var sentence = Console.ReadLine();
+			var match = Regex.Replace(sentence, word, new string('*', word.Length));
+			Console.WriteLine(match);
+		}
+	}
+}
---
+++ EmailMe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailMe
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = Console.ReadLine().Split('@');
+			var first = input[0].Sum(x => (int)x);
+			var second = input[1].Sum(x => (int)x);
+			var result = first - second;
+
+			if (result >= 0)
+			{
+				Console.WriteLine("Call her!");
+			}
+			else
+			{
+				Console.WriteLine("She is not the one.");
+			}
+		}
+	}
+}
