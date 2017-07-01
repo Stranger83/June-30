d19d6f500dc328523746141cf4242b9b84af0938
July Manova <jmanova83@gmail.com>
2017-07-01 18:07:06 +0300

    03,06/08 solved

---
+++ EmailStatistics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailStatistics
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var n = int.Parse(Console.ReadLine());
+			var pattern = @"(?<user>[a-zA-Z]{5,})@(?<host>[a-z]{3,}\.(bg|com|org)$)";
+			var emailDict = new Dictionary<string, List<string>>();
+			for (int i = 0; i < n; i++)
+			{
+				var email = Regex.Match(Console.ReadLine(), pattern);
+				if (email.Success)
+				{
+					if (!emailDict.ContainsKey(email.Groups["host"].Value))
+					{
+						emailDict[email.Groups["host"].Value] = new List<string>();
+					}
+					if (!emailDict[email.Groups["host"].Value].Contains(email.Groups["user"].Value))
+					{
+						emailDict[email.Groups["host"].Value].Add(email.Groups["user"].Value);
+					}
+				}
+			}
+									
+			foreach (var kvp in emailDict.OrderByDescending(x => x.Value.Count))
+			{
+				var host = kvp.Key;
+				var users = kvp.Value;
+				Console.WriteLine($"{host}:");
+				foreach (var user in users)
+				{
+					Console.WriteLine($"### {user}");
+				}
+			}
+		}
+	}
+}
---
+++ KarateStrings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarateStrings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = Console.ReadLine();
+			Console.WriteLine(Punch(input));
+		}
+
+		private static string Punch(string input)
+		{
+			int power = 0;
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (input[i] == '>')
+				{
+					power += int.Parse(input[i + 1].ToString());
+
+					while (power > 0 && i + 1 < input.Length)
+					{
+						if (input[i+1] == '>')
+						{
+							break;
+						}
+						input = input.Remove(i + 1, 1);
+						power--;
+					}
+				}
+			}
+			return input;
+		}
+	}
+}
